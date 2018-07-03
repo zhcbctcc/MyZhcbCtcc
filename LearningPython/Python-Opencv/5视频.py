20d350eebff85918e79e093885e3963723619748
zhcbctcc <39824863+zhcbctcc@users.noreply.github.com>
2018-07-04 07:01:02 +0800

    Update 5视频.py

--- LearningPython/Python-Opencv/5视频.py
+++ LearningPython/Python-Opencv/5视频.py
@@ -12,3 +12,29 @@ import numpy as np
 #
 # cap.release()
 # cv2.destroyAllWindows()
+
+#第四个程序：从摄像头中捕捉视频，沿水平方向旋转每一帧并保存
+# import numpy as np
+# import cv2
+# 
+# cap = cv2.VideoCapture(0)
+# 
+# fourcc = cv2.VideoWriter_fourcc(*'XVID')
+# out = cv2.VideoWriter('output.avi',fourcc,20.0,(640,480))
+# 
+# while(cap.isOpened()):
+#     ret,frame = cap.read()
+#     if ret == True:
+#         frame = cv2.flip(frame,0)
+# 
+#         out.write(frame)
+# 
+#         cv2.imshow('frame',frame)
+#         if cv2.waitKey(1) &0xFF == ord('q'):
+#             break
+#         else:
+#             break
+# 
+# cap.release()
+# out.release()
+# cv2.destroyAllWindows()
