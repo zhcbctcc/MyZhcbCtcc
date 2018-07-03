dd4558d6be2630d5e7e4e90043497de0563da546
zhcbctcc <39824863+zhcbctcc@users.noreply.github.com>
2018-07-03 17:21:42 +0800

    Update 10个有趣的项目.py

--- LearningPython/10个有趣的项目.py
+++ LearningPython/10个有趣的项目.py
@@ -35,3 +35,29 @@ print (u'Info:',image_file.size[0],' ',image_file.size[1],' ',count)
 tmp = open('tmp.txt','w')
 tmp.write(transform1(image_file))
 tmp.close()
+
+# #第二个程序：生成分形图片
+# import pygame, math
+# 
+# pygame.init()
+# window = pygame.display.set_mode((600, 600))
+# pygame.display.set_caption("Fractal Tree")
+# screen = pygame.display.get_surface()
+# 
+# def drawTree(x1, y1, angle, depth):
+#     if depth:
+#         x2 = x1 + int(math.cos(math.radians(angle)) * depth * 10.0)
+#         y2 = y1 + int(math.sin(math.radians(angle)) * depth * 10.0)
+#         pygame.draw.line(screen, (255,255,255), (x1, y1), (x2, y2), 2)
+#         drawTree(x2, y2, angle - 20, depth - 1)
+#         drawTree(x2, y2, angle + 20, depth - 1)
+# 
+# def input(event):
+#     if event.type == pygame.QUIT:
+#         exit(0)
+# 
+# drawTree(300, 550, -90, 9)
+# pygame.display.flip()
+# while True:
+#     input(pygame.event.wait())
+
