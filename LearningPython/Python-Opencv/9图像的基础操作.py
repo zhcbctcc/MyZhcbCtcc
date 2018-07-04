3315a1a3d5ef2af4229c16407dbed20b4a27eb4e
zhcbctcc <39824863+zhcbctcc@users.noreply.github.com>
2018-07-04 16:54:17 +0800

    Create 9图像的基础操作.py

---
+++ LearningPython/Python-Opencv/9图像的基础操作.py
@@ -0,0 +1,20 @@
+9.1 获取并修改像素值
+import cv2
+# import numpy as np
+# img = cv2.imread('A.jpg')
+# px = img[100,100]
+# print(px)
+# blue = img[100,100,0]
+# print(blue)
+# 
+# # img[100,100] = [255,255,255]
+# # print(img[100,100])
+
+9.2
+# #9.2获取图像属性,像素等
+# import cv2
+# import numpy as np
+# img = cv2.imread('A.jpg')
+# print(img.shape)
+# print(259*463*3)
+# print(img.size)
